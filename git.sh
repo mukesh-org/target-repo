278d496202150ddc95d094c6ade1a9b0c7cbf13f
kartikey.sarode <kartikey.sarode@searce.com>
2019-01-09 13:48:26 +0530

    initial upload

---
+++ git.sh
@@ -0,0 +1,12 @@
+### Shell script for performing GitHub commands ###
+
+#!/bin/sh
+
+git remote add origin https://github.com/mukesh-org/config-repo.git
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+branch=$(date +%s)
+git checkout -b "$branch"
+git add .
+git commit -m 'kustomize file updated'
+git push origin "$branch"
