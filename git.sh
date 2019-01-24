bd91e4d00481a607addecad6ed75a7bd9c74fb71
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-01-24 17:21:19 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -5,7 +5,7 @@
 git remote add origin git@github.com:mukesh-org/config-repo.git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
-branch=$(PR-name)
+branch=getenv('PULL_NUMBER')
 git checkout -b "$branch"
 git add .
 git commit -m 'kustomize file updated'
