fc6a5491fa877e4d06f06d3dfd3b8f5b38d02a30
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-01-25 16:13:25 +0530

    Create gitmerge.sh

---
+++ gitmerge.sh
@@ -0,0 +1,19 @@
+### Shell script for performing GitHub merge commands to master branch ###
+
+#!/bin/sh
+
+git remote add origin git@github.com:mukesh-org/config-repo.git
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+branch=$PULL_NUMBER
+git checkout -b "$branch"
+git add .
+git commit -m 'kustomize file updated'
+git push origin "$branch"
+
+## resolve any merge conflicts if there are any
+git fetch origin master
+git merge FETCH_HEAD
+
+git checkout master
+git merge --no-ff "$branch"
