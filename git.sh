db751cc1b59cd4cd0184e02a29e8f86b1b043b21
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-01-28 19:38:26 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,6 +1,13 @@
-### Shell script for performing GitHub commands ###
+### Shell script for performing GitHub commands to merge content to PR named branch ###
 
 #!/bin/sh
+
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+
+cp /secrets/flux/id_rsa /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
 
 git remote add origin git@github.com:$GITHUB_ORG_NAME/$TARGET_REPO_NAME.git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
