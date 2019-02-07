7b2de1065ea02323101087f489fbf1dfcc36b1ae
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-07 22:29:47 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -9,6 +9,9 @@ cp /secrets/flux/id_rsa /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 
+git config --global user.email "you@example.com"
+git config --global user.name "ci-robot"
+
 git remote add origin git@github.com:$GITHUB_ORG_NAME/$TARGET_REPO_NAME.git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
