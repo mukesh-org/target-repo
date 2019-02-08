4044163f111b150c92618ec32b498b9c04d1ea40
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-08 17:31:15 +0530

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,18 +2,10 @@
 
 #!/bin/sh
 
-mkdir /root/.ssh
-chmod 700 /root/.ssh
-
-cp /secrets/flux/id_rsa /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
-
 git config --global user.email "you@example.com"
 git config --global user.name "ci-robot"
 
 git remote add origin git@github.com:$GITHUB_ORG_NAME/$TARGET_REPO_NAME.git
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
 branch=$PULL_NUMBER
 git checkout -b "$branch"
