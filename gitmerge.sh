756c3517cf8d16b29bdd9499fc96be985c60ea87
mukesh-panigrahi <33598282+mukesh-panigrahi@users.noreply.github.com>
2019-02-08 17:55:30 +0530

    Update gitmerge.sh

--- gitmerge.sh
+++ gitmerge.sh
@@ -1,30 +1,27 @@
 ### Shell script for performing GitHub merge commands to master branch ###
 
 #!/bin/sh
+set -e
 
 mkdir /root/.ssh
 chmod 700 /root/.ssh
 
-cp /secrets/flux/id_rsa /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-ssh-add ~/.ssh/id_rsa
+cp /secrets/git/target_ssh_secret /root/.ssh/target_ssh_secret
+chmod 600 /root/.ssh/target_ssh_secret
+ssh-add /root/.ssh/target_ssh_secret
 
 cat <<\EOF >> ~/.ssh/config
 Host $TARGET_REPO_NAME github.com
 	HostName github.com
 	User git
-	IdentityFile ~/.ssh/id_rsa
+	IdentityFile ~/.ssh/target_ssh_secret
 EOF
 chmod 400 ~/.ssh/config
 
-git remote add origin git@github.com:$GITHUB_ORG_NAME/$TARGET_REPO_NAME.git
+git remote add origin git@github.com:"$GITHUB_ORG_NAME"/"$TARGET_REPO_NAME".git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
-branch=$PULL_NUMBER
-git checkout -b "$branch"
-git add .
-git commit -m 'kustomize file updated'
-git push origin "$branch"
+branch="$PULL_NUMBER"
 
 ## resolve any merge conflicts if there are any
 git fetch origin master
